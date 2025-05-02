9d439983e444a9ee095cb3e8006defc34800dfa6
Istvan <istvansantaa@gmail.com>
2025-05-02 11:36:12 +0200

    carouselle ok

--- assets/scripts.js
+++ assets/scripts.js
@@ -12,4 +12,20 @@ $(document).ready(function() {
         showTags: true,
         tagsPosition: 'top'
     });
+
+    $.fn.mauGallery.methods.prevImage = function(lightboxId) {
+        const images = $(".gallery-item:visible");
+        let currentSrc = $("#" + lightboxId + " .lightboxImage").attr("src");
+        let currentIndex = images.toArray().findIndex(img => $(img).attr("src") === currentSrc);
+        let prevIndex = (currentIndex - 1 + images.length) % images.length;
+        $("#" + lightboxId + " .lightboxImage").attr("src", $(images[prevIndex]).attr("src"));
+    }
+
+    $.fn.mauGallery.methods.nextImage = function(lightboxId) {
+        const images = $(".gallery-item:visible");
+        let currentSrc = $("#" + lightboxId + " .lightboxImage").attr("src");
+        let currentIndex = images.toArray().findIndex(img => $(img).attr("src") === currentSrc);
+        let nextIndex = (currentIndex + 1) % images.length;
+        $("#" + lightboxId + " .lightboxImage").attr("src", $(images[nextIndex]).attr("src"));
+    }
 });
